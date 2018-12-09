e47994db9db5053f9ff60cd9ad42b2ba2c850eea
rockkoca <rockkoca@gmail.com>
2018-12-08 20:16:58 -0800

    Update tmux_config.sh

--- tmux_config.sh
+++ tmux_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir ~/.tmux
-wget https://raw.githubusercontent.com/rockkoca/tmux_config/master/.tmux.conf ~/.tmux.conf
+wget https://raw.githubusercontent.com/rockkoca/tmux_config/master/.tmux.conf -O ~/.tmux.conf
 # tmux source ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
